a513e6a0942646e03deb53f93482c9e5ab10dea0
Jeff Dutra <jeffdutra@outlook.com>
2015-04-19 08:39:34 -0400

    Added projects for the linting and browserify

---
+++ BrowserifyCommonJs/js/library/src/numberGreaterThanOrEqualTo.js
@@ -0,0 +1,12 @@
+var numberGreaterThanOrEqualTo = function( value, testValue ){
+        if(isNaN(value)){
+            return false;
+        }
+        if(isNaN(testValue)){
+            return true;
+        }
+
+        return Number(value) >= Number(testValue);
+};
+
+module.exports = numberGreaterThanOrEqualTo;
---
+++ CodeLinting/js/library/src/main.js
@@ -0,0 +1,7 @@
+var numberGreaterThanOrEqualTo = require('./numberGreaterThanOrEqualTo');
+
+console.log(numberGreaterThanOrEqualTo(1,2));
+
+console.log(numberGreaterThanOrEqualTo(1,'dd'));
+
+console.log(numberGreaterThanOrEqualTo(4,2));
