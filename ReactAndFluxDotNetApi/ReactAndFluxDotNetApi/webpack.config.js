09f5b2047088a2d9592c26cf83587f6fc8749c22
Jeff Dutra <jeffdutra@outlook.com>
2015-06-15 20:10:54 -0400

    Added more webpack stuff

---
+++ ReactAndFluxDotNetApi/ReactAndFluxDotNetApi/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var WebPackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+  entry: './js/library/src/components/main.jsx',
+  output: {
+    filename: 'dist/app.js'
+  },
+  module:{
+    loaders: [
+      {test: /\.jsx$/, loader:'jsx-loader'}
+    ],
+    preLoaders: [
+      {
+        test: /\.js$/, // include .js files
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        loader: "jshint-loader"
+      },
+      {
+        test: /\.js$/, // include .js files
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        loader: "jscs-loader"
+      }
+    ]
+  },
+  plugins: [
+    new WebPackNotifierPlugin({title: 'Build'})
+  ],
+  resolve: {
+    // you can now require('file') instead of require('file.coffee')
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  externals:{
+    'jquery': 'jquery'
+  }
+};
